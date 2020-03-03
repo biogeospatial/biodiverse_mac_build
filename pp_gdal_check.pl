d20ee9a4b7c13f1da2913b3308484daffdb3ba89
Shawn Laffan <shawnlaffan@gmail.com>
2020-03-03 12:13:20 +1100

    separate gdal pack test, using otool

--- pp_gdal_check.pl
+++ pp_gdal_check.pl
@@ -5,11 +5,32 @@ use warnings;
 use Alien::gdal;
 
 #say join ":", Alien::gdal->dynamic_libs;
+my @libs_to_pack;
+my %seen;
 
 foreach my $lib (Alien::gdal->dynamic_libs) {
     say "otool -L $lib"; 
-    system ('otool', '-L', $lib);
+    my $libs = system ('otool', '-L', $lib);
+    my @lib_arr = split "\n", $libs;
+    shift @lib_arr;  #  first result is alien dylib
+    foreach my $line (@lib_arr) {
+        my @fields = split /\s+/, $line;
+        next if $seen{$fields[0]};
+        push @libs_to_pack, $fields[0];
+        $seen{$fields[0]}++;
+    }
+    
 }
 
-eval 'require Geo::GDAL::FFI'
-  or warn 'could not load Geo::GDAL::FFI';
+my @inc_to_pack = map {('--link' => $_)} @libs_to_pack;
+
+system (
+    'pp',
+    '-u',
+    '-B',
+    '-x',
+    @inc_to_pack,
+    '-o',
+    'pp_gdal_check',
+    'pp_gdal_check.pl',
+);
---
+++ test_script.pl
@@ -0,0 +1,12 @@
+use 5.020;
+use strict;
+use warnings;
+
+use Alien::gdal;
+
+eval 'use Geo::GDAL::FFI'
+  or warn "unable to load Geo::GDAL::FFI";
+
+say join "\n", Alien::gdal->dynamic_libs;
+
+1;
