0ea03ebdc7fb95dec1cfea06d00b80b7fd43be69
Shawn Laffan <shawnlaffan@gmail.com>
2020-03-03 12:50:10 +1100

    a bit more direct wrt FFI and GDAL

--- test_script.pl
+++ test_script.pl
@@ -2,11 +2,38 @@ use 5.020;
 use strict;
 use warnings;
 
-use Alien::gdal;
+package FFICHECK {
 
-eval 'require Geo::GDAL::FFI'
-  or warn "unable to load Geo::GDAL::FFI";
+    use Carp;
+    use FFI::Platypus;
+    use FFI::Platypus::Buffer;
+    
+    use Alien::gdal;
+    
+    #eval 'require Geo::GDAL::FFI'
+    #  or warn "unable to load Geo::GDAL::FFI";
+    
+    say join "\n", Alien::gdal->dynamic_libs;
+    
+    
+    my $ffi = FFI::Platypus->new;
+    $ffi->load_custom_type('::StringPointer' => 'string_pointer');
+    $ffi->lib(Alien::gdal->dynamic_libs);
+    
+    $ffi->type('(pointer,size_t,size_t,opaque)->size_t' => 'VSIWriteFunction');
+    $ffi->type('(int,int,string)->void' => 'CPLErrorHandler');
+    $ffi->type('(double,string,pointer)->int' => 'GDALProgressFunc');
+    $ffi->type('(pointer,int, pointer,int,int,unsigned int,unsigned int,int,int)->int' => 'GDALDerivedPixelFunc');
+    $ffi->type('(pointer,int,int,pointer,pointer,pointer,pointer)->int' => 'GDALTransformerFunc');
+    $ffi->type('(double,int,pointer,pointer,pointer)->int' => 'GDALContourWriter');
+    
+    # from port/*.h
+    eval{$ffi->attach(VSIMalloc => [qw/uint/] => 'opaque');};
+    warn $@ if $@;
+    croak "Can't attach to GDAL methods. Does Alien::gdal provide GDAL dynamic libs?" unless FFICHECK->can('VSIMalloc');
+    
+    1;
 
-say join "\n", Alien::gdal->dynamic_libs;
+};
 
 1;
