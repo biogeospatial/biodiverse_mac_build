dc1117085078f4124c6ea788440c819dfc819ae9
Shawn Laffan <shawnlaffan@gmail.com>
2022-11-16 15:33:56 +1100

    mmb3.pl: Remove code that is now handled by pp_autolink

--- bin/mmb3.pl
+++ bin/mmb3.pl
@@ -119,150 +119,7 @@ my @hard_coded_dylibs = (
     #'$ENV{HOMEBREW_PREFIX}/Cellar/libgnomecanvas/2.30.3_5/lib/libgnomecanvas-2.0.dylib',
 );
 
-#  find dependent dlls
-#  could also adapt some of Module::ScanDeps::_compile_or_execute
-#  as it handles more edge cases
-sub get_dep_dlls {
-    my ($script, $no_execute_flag) = @_;
 
-    #  This is clunky:
-    #  make sure $script/../lib is in @INC
-    #  assume script is in a bin folder
-    my $rlib_path = (path ($script)->parent->stringify) . '/lib';
-    #say "======= $rlib_path/lib ======";
-    local @INC = (@INC, $rlib_path)
-      if -d $rlib_path;
-
-    my $deps_hash = scan_deps(
-        files   => [ $script ],
-        recurse => 1,
-        execute => 1, #!$no_execute_flag,
-        #cache_file => $cache_file,
-    );
-
-    my @lib_paths
-      = reverse sort {length $a <=> length $b}
-        map {path($_)->absolute}
-        @INC;
-
-    my $paths = join '|', map {quotemeta} @lib_paths;
-    my $inc_path_re = qr /^($paths)/i;
-    #say $inc_path_re;
-
-    my $RE_DLL_EXT = qr/\.($Config::Config{so}|bundle)$/i;
-    # $RE_DLL_EXT = qr/\.bundle$/;
-
-    my %dll_hash;
-    foreach my $package (keys %$deps_hash) {
-        #  could access {uses} directly, but this helps with debug
-        my $details = $deps_hash->{$package};
-        my $uses    = $details->{uses};
-        next if !$uses;
-
-        foreach my $dll (grep {$_ =~ $RE_DLL_EXT} @$uses) {
-            my $dll_path = $deps_hash->{$package}{file};
-            #  Remove trailing component of path after /lib/
-            if ($dll_path =~ m/$inc_path_re/) {
-                $dll_path = $1 . '/' . $dll;
-            }
-            else {
-                #  fallback, get everything after /lib/
-                $dll_path =~ s|(?<=/lib/).+?$||;
-                $dll_path .= $dll;
-            }
-            #say $dll_path;
-            croak "either cannot find or cannot read $dll_path "
-                . "for package $package"
-              if not -r $dll_path;
-            $dll_hash{$dll_path}++;
-        }
-    }
-
-    my @dll_list = sort keys %dll_hash;
-    return wantarray ? @dll_list : \@dll_list;
-}
-
-sub find_so_files {
-    my $target_dir = shift or die;
-
-    my @files = File::Find::Rule->extras({ follow => 1, follow_skip=>2 })
-                             ->file()
-                             ->name( qr/\.so$/ )
-                             ->in( $target_dir );
-    return wantarray ? @files : \@files;
-}
-
-sub get_inc_to_pack {
-    my $target_script = shift;
-
-    say '+++++';
-    say 'Finding XS bundle files';
-    my @bundle_list = get_dep_dlls($target_script);
-    #say join ' ', @bundle_list;
-    #system ('otool', '-L', $bundle_list[0]);
-    say '+++++';
-
-    my @libs_to_pack = (
-        @hard_coded_dylibs,
-    );
-    my %seen;
-
-    my @target_libs = (
-        @hard_coded_dylibs,
-        Alien::gdal->dynamic_libs,
-        @bundle_list,
-        '/usr/local/opt/libffi/lib/libffi.6.dylib',
-        $pixbuf_query_loader,
-        find_so_files ($gdk_pixbuf_dir),
-    );
-    while (my $lib = shift @target_libs) {
-        say "otool -L $lib";
-        my @lib_arr = qx /otool -L $lib/;
-        warn qq["otool -L $lib" failed\n]
-          if not $? == 0;
-        shift @lib_arr;  #  first result is dylib we called otool on
-        foreach my $line (@lib_arr) {
-            $line =~ /^\s+(.+?)\s/;
-            my $dylib = $1;
-            next if $seen{$dylib};
-            next if $dylib =~ m{^/System};  #  skip system libs
-            #next if $dylib =~ m{^/usr/lib/system};
-            next if $dylib =~ m{^/usr/lib/libSystem};
-            next if $dylib =~ m{^/usr/lib/};
-            next if $dylib =~ m{\Qdarwin-thread-multi-2level/auto/share/dist/Alien\E};  #  another alien
-            say "adding $dylib for $lib";
-            push @libs_to_pack, $dylib;
-            $seen{$dylib}++;
-            #  be paranoid in case otool does not get the full set
-            push @target_libs, $dylib;
-        }
-    }
-
-    #my @inc_to_pack
-    #  = map {('--link' => $_)}
-    #    (@libs_to_pack, '/usr/local/opt/libffi/lib/libffi.6.dylib');
-    my @inc_to_pack;
-    foreach my $file (@libs_to_pack) {
-        my $basename = path($file)->basename;
-        while (-l $file) {
-            my $linked_file = readlink ($file);
-            say "$file is a symbolic link that points to $linked_file";
-            #$file = _chase_lib_darwin($file);
-
-            #  handle relative paths, or symlinks to sibling files
-            if (!path($linked_file)->is_absolute) {
-
-                my $file_path = path($file)->parent->stringify;
-                $linked_file = path("$file_path/$linked_file")->stringify;
-            }
-            $file = $linked_file;
-        }
-        push @inc_to_pack, ("-a" => "$file\;../" . $basename);
-    }
-
-    return @inc_to_pack;
-
-}
 
 
 ###########################################
@@ -314,32 +171,13 @@ my $output_binary_fullpath = path ($out_folder, $output_binary)->absolute;
 my $icon_file_base = $icon_file ? basename ($icon_file) : '';
 my @icon_file_arg  = $icon_file ? ('-a', "$icon_file\;$icon_file_base") : ();
 
-###########################################
+# ###########################################
+# #
+# # Run the constructed pp command.
+# #
+# ###########################################
 #
-# Run the constructed pp command.
-#
-###########################################
 
-#  we need to use -M with the aliens,
-#  and possibly some others
-my @aliens = qw /
-    Alien::gdal       Alien::geos::af
-    Alien::proj       Alien::sqlite
-    Alien::libtiff    Alien::curl
-    Alien::spatialite Alien::freexl
-    File::ShareDir
-/;
-#    Alien::spatialite Alien::freexl
-#/;
-#  we don't always have all of the aliens installed
-foreach my $alien (@aliens) {
-    if (eval "require $alien") {
-        push @rest_of_pp_args, '-M' => $alien;
-    }
-}
-
-#  this is handled by pp_autolink now
-my @inc_to_pack = get_inc_to_pack ($script_fullname);
 
 my @verbose_command = $verbose ? ("-v") : ();
 
